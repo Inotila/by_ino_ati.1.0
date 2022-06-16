3c0793ac7a634a2c51da6cfd9233f60962bef9f7
Inotila <inoati1991@gmail.com>
2022-06-16 13:31:44 +0000

    initial commit

---
+++ by_ino_ati/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for by_ino_ati project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'by_ino_ati.settings')
+
+application = get_asgi_application()
---
+++ by_ino_ati/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for by_ino_ati project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'by_ino_ati.settings')
+
+application = get_wsgi_application()
