7f0014cd92403814314e231aa8a610d32f8ad187
Inotila <inoati1991@gmail.com>
2022-07-06 10:55:16 +0000

    added views to display all are and created a urls file to include as path to the project level

--- art/views.py
+++ art/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Art
 
 # Create your views here.
+
+
+def art_display(request):
+    """
+    render a display of all the art
+    """
+
+    art = Art.objects.all()
+    
+    context = {
+        'art': art,
+    }
+
+    return render(request, 'art/art.html', context)
