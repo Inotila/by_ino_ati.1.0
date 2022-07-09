9513b403733f446462c5b644303997c578c0df8b
Inotila <inoati1991@gmail.com>
2022-07-09 17:52:55 +0000

    Added env important to prevent error because env can't be found.

--- by_ino_ati/settings.py
+++ by_ino_ati/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
