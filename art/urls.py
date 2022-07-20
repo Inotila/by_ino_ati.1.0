75f9e332af612fff74e98e944787086f554225ac
Inotila <inoati1991@gmail.com>
2022-07-20 17:09:17 +0000

    Added details url path

--- art/urls.py
+++ art/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.art_display, name='art')
+    path('', views.art_display, name='art'),
+    path('title/', views.art_detail, name='details')
 ]
