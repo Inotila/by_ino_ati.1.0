dff0246ed2d380a1a0c1da8a957eb5a829b27c43
Inotila <inoati1991@gmail.com>
2022-07-23 15:01:04 +0000

    Deleted the url path for details as it points to the same path as art

--- by_ino_ati/urls.py
+++ by_ino_ati/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('art/', include('art.urls')),
-    path('details/', include('art.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
