e7eb6ad553c252a72d85d7326951914b3fcd883a
Inotila <inoati1991@gmail.com>
2022-07-25 13:37:26 +0000

    Added imports search functionality, conditional is set on line 16 so when search category is not part of the search

--- art/views.py
+++ art/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Art, Category
 
 # Create your views here.
@@ -8,12 +10,29 @@ def art_display(request):
     """
     render a display of all the art
     """
-    category = request.GET['category']
-    art = Art.objects.filter(category=category)
+    art = None
+    query = None
+    category = None
+    if request.GET:
+        if 'category' in request.GET:
+            category = request.GET['category']
+            art = Art.objects.filter(category=category)
+
+    if request.GET:
+        if 'art-search' in request.GET:
+            query = request.GET['art-search']
+            queries = Q(title__icontains=query)
+            art = Art.objects.filter(queries)
+            print(f'art is {art}')
+            if not art:
+                print('no art')
+                messages.error(request, "not found")
+                return redirect(reverse('art'))
 
     context = {
         'art': art,
         'category': category,
+        'search_term': query
     }
 
     return render(request, 'art/art.html', context)
