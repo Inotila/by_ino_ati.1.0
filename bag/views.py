36a41981f254dbf66c05011ab64b29c37c414946
Inotila <inoati1991@gmail.com>
2022-07-28 15:15:51 +0000

    Added ad to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,21 @@ from django.shortcuts import render
 def view_bag(request):
     """ A view that renders the bags contents page """
     return render(request, 'bag/bag.html')
+
+
+def add_art_to_bag(request, id):
+    """ Add art piece to shopping bag"""
+
+    quantity = 1
+    redirect_url = request.POST.GET(redirect_url)
+    added_to_bag_art = get_object_or_404(Art, pk=id)
+    bag = request.session.get('bag', {})
+
+    if id in list(bag.key()):
+        bag[id] += quantity
+    else:
+        bag[id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
