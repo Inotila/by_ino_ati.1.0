639e838a97112a29950cce997259f7f31a0ac3a7
Inotila <inoati1991@gmail.com>
2022-07-29 12:21:05 +0000

    Update url path

--- bag/urls.py
+++ bag/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
-    path('add/<int:art_id>/', views.add_art_to_bag, name='add_art_to_bag'),
+    path('add/<art_id>/', views.add_to_bag, name='add_to_bag'),
 ]
