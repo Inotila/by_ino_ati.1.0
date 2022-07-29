ac6b1d392b18a8f9ec5b1eb35d617d3bbb5589e1
Inotila <inoati1991@gmail.com>
2022-07-29 15:50:05 +0000

    Added a processor for the context

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from art.models import Art
 
 
 def bag_contents(request):
@@ -9,6 +11,17 @@ def bag_contents(request):
     total = 0
     art_piece_count = 0
     delivery = 1000
+    bag = request.session.get('bag', {})
+
+    for art_id, quantity in bag.items():
+        art = get_object_or_404(Art, pk=art_id)
+        total += art.price
+        art_piece_count += quantity
+        bag_items.append({
+            'art_id': art_id,
+            'quantity': quantity,
+            'art': art
+        })
 
     grand_total = total + delivery
 
