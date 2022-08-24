82c3b6676ab681d5d971e61fc4ef57c2ed48da92
Inotila <inoati1991@gmail.com>
2022-08-24 12:10:16 +0000

    Added a form data to store the users data when they submit

--- checkout/views.py
+++ checkout/views.py
@@ -10,26 +10,40 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag')
-    if not bag:
-        messages.error(request, "Your bag is empty")
-        return redirect(reverse('art'))
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+    else:
+        bag = request.session.get('bag')
+        if not bag:
+            messages.error(request, "Your bag is empty")
+            return redirect(reverse('art'))
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reverse('products'))
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('products'))
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
-    order_form = OrderFrom()
+        order_form = OrderFrom()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
