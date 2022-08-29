c6113975b7ff60691fa0b387adc359331c02f0b6
Inotila <inoati1991@gmail.com>
2022-08-29 17:46:42 +0000

    Added variables to store user detail to create an order

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -21,7 +21,14 @@ class StripeWH_Handler:
         """
 
         intent = event.data.object
-        print(intent)
+        pid = intent.id
+        bag = intent.metadata.bag
+        save_info = intent.metadata.save_info
+
+        billing_details = intent.charges.data[0].billing_details
+        shipping_details = intent.shipping
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
+
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
