6fa51941ffbe925a9e96002322de4c86599b5286
Inotila <inoati1991@gmail.com>
2022-08-29 13:51:51 +0000

    Added webhook listner

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_done<order_number>', views.checkout_done, name='checkout_done'),
+    path('wh/', webhook, name='webhook')
 ]
