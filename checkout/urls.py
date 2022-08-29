335bce33cea4f0c394fded3a1a1f30cf0217ed3d
Inotila <inoati1991@gmail.com>
2022-08-29 14:13:09 +0000

    Added checkout data view

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,5 +6,6 @@ from .webhooks import webhook
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_done<order_number>', views.checkout_done, name='checkout_done'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook')
 ]
