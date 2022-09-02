419011be70776325f992cb7bb4c5892599e9422a
Inotila <inoati1991@gmail.com>
2022-09-02 15:16:16 +0000

    Updated template profile and checkoutview to save the users order hisroty on to their profile

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from .forms import OrderFrom
 from .models import Order, OrderLineItem
 from art.models import Art
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from bag.contexts import bag_contents
 import stripe
 import json
@@ -115,6 +117,27 @@ def checkout_done(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    profile = UserProfile.objects.get(user=request.user)
+    # Attach the user's profile to the order
+    order.user_profile = profile
+    order.save()
+
+    # Save the user's info
+    if save_info:
+        profile_data = {
+            'default_phone_number': order.phone_number,
+            'default_street_address1': order.street_address1,
+            'default_street_address2': order.street_address2,
+            'default_postcode': order.postcode,
+            'default_town_or_city': order.town_or_city,
+            'default_county': order.county,
+            'default_country': order.country,
+        }
+        user_profile_form = UserProfileForm(profile_data, instance=profile)
+        if user_profile_form.is_valid():
+            user_profile_form.save()
+
     messages.success(request, f'Purchase successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
