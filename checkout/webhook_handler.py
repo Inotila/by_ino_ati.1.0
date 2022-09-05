53a2f90a3bc33c54d8858e49b4cf811089d3af56
Inotila <inoati1991@gmail.com>
2022-09-05 09:42:08 +0000

    Added sending email functionality

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -98,6 +98,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
@@ -133,6 +134,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
