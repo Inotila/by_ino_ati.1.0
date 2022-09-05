25407b474c0fc760a09f9bb5ddb9886ab75c2294
Inotila <inoati1991@gmail.com>
2022-09-05 16:26:26 +0000

    Updated debug in settings

--- by_ino_ati/settings.py
+++ by_ino_ati/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['byinoati.herokuapp.com', 'localhost']
 
