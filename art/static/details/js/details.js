b4e520be2b7e61cafdbbb1e486018b0e5191d0cb
Inotila <inoati1991@gmail.com>
2022-11-18 20:37:43 +0000

    Add style and js to share overlay to implement open and close function

--- art/static/details/js/details.js
+++ art/static/details/js/details.js
@@ -1,3 +1,5 @@
+// full image view
+
 const overlay = document.getElementById("full-img-overlay");
 overlay.style.visibility = 'hidden'
 
@@ -14,4 +16,19 @@ document.getElementById("close-full-img-icon").addEventListener("click", functio
   document.getElementById("full-img-text-btn").addEventListener("click", function(event){
     event.preventDefault();
      overlay.style.visibility = 'visible';
+  });
+
+  // share button
+
+  const shareOverlay = document.getElementById("share-div-overlay");
+  shareOverlay.style.visibility = 'hidden'
+
+  document.getElementById("share-btn").addEventListener("click", function(event){
+    event.preventDefault();
+    shareOverlay.style.visibility = 'visible';
+  });
+
+  document.getElementById("close-share-div-icon").addEventListener("click", function(event){
+    event.preventDefault();
+    shareOverlay.style.visibility = 'hidden';  
   });
