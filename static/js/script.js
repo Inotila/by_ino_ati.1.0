4479ec0100023d5bc402210d0fbf3a59469dbf5f
Inotila <inoati1991@gmail.com>
2022-11-18 12:30:22 +0000

    Added cookies consent popup

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,41 @@ setTimeout(function () {
     let alert = new bootstrap.Alert(messages);
     alert.close();
 }, 7000);
+
+// cookies consent
+
+const cookieStorage = {
+    getItem: (key) => {
+        const cookies = document.cookie
+        .split(';')
+        .map(cookie => cookie.split('='))
+    .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }),{});
+    return cookies[key];
+    },
+    setItem: (key, value) => {
+        document.cookie = `${key}=${value}`
+    },
+};
+
+const storageType = cookieStorage;
+const consentPropertyName = 'byinoati_consent';
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () => {
+    const consentPopup = document.getElementById('consent-popup');
+    const acceptBtn = document.getElementById('accept');
+
+    const acceptFn = event => {
+        saveToStorage(storageType)
+        consentPopup.classList.add('hidden');
+    }
+
+    acceptBtn.addEventListener('click', acceptFn);
+
+    if (shouldShowPopup(storageType)){
+        setTimeout(() => {
+            consentPopup.classList.remove('hidden');
+        }, 2000);
+    }
+};
