a22ec461929a563e437439d80e061e040d330eaf
Inotila <inoati1991@gmail.com>
2022-11-30 17:36:24 +0000

    Added social account providers for facebook and google

--- by_ino_ati/settings.py
+++ by_ino_ati/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
     'home',
     'art',
     'bag',
