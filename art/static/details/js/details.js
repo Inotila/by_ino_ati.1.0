e134fa093eb85de5df14eddccb3eed29ec61109a
Inotila <inoati1991@gmail.com>
2022-12-08 22:05:04 +0000

    Added boundry limitation for mouse when zooming over image

--- art/static/details/js/details.js
+++ art/static/details/js/details.js
@@ -52,16 +52,42 @@ document.getElementById("close-full-img-icon").addEventListener("click", functio
 
   container.addEventListener('mousemove', zoomImage)
 
+  result.style.backgroundImage = `url(${normalImage.src})`
+
   function zoomImage(e){
-    console.log('zoom image', e.clientX, e.clientY)
-
-    let x = e.clientX - containerRect.left - lensRect.width / 2;
-    let y = e.clientY - containerRect.top - lensRect.height / 2;
-
-    let minX = 0;
-    let minY = 0;
-    let maxX = containerRect.width - lensRect.width;
+    const { x, y} = getMousePosition(e);
 
     lens.style.left = x + "px";
     lens.style.top = y + "px";
+
+    let fx = resultRect.width / lensRect.width 
+    let fy = resultRect.height / lensRect.height     
+
+    result.style.backgroundSize = `${imageRect.width * fx}px ${imageRect.height * fy}px`;
+    result.style.backgroundPosition = `-${x * fx}px -${y * fy}px`
+  }
+
+  function getMousePosition(e){
+    let x = e.clientX - containerRect.left - lensRect.width / 2;
+    let y = e.clientY - containerRect.top - lensRect.height / 2;
+
+    // limits the lens/mouse in the box of the image
+    let minX = 0;
+    let minY = 0;
+    let maxX = containerRect.width - lensRect.width;
+    let maxY = containerRect.height - lensRect.height;
+
+    if(x <= minX){
+      x = minX;
+    } else if ( x >= maxX){
+      x = maxX;
+    }
+
+    if(y <= minY){
+      y = minY;
+    } else if ( y >= maxY){
+      y = maxY;
+    }
+
+    return{ x, y}
   }
