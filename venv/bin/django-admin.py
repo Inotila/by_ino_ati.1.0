2214a9404d9221ed7bbaf50ff5fddbe40bdaf42b
inoati1991@gmail.com <inoati1991@gmail.com>
2025-04-15 10:26:50 +0200

    Switched to psycopg2-binary for easier local setup

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,21 @@
+#!/Users/Len/Documents/keys/inovative studios/Apps/byinoati.com/by_ino_ati.1.0/venv/bin/python3
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
